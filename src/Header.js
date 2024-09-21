0d924ca5eaa1383a433bcd097d5fc91e7ada03f2
Madiseng <Lee.Madiseng@afgri.co.za>
2024-09-21 18:21:03 +0200

    Commit message describing your changes

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+      <header>
+        <img src="logo.png" alt="Little Lemon Logo" />
+        {/* You can replace "logo.png" with the actual path to your logo */}
+      </header>
+    );
+  }
+  
+  export default Header;
+  
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+function Main() {
+    return (
+      <main>
+        <h1>Welcome to Little Lemon</h1>
+        <p>This is the main content of the website.</p>
+      </main>
+    );
+  }
+  
+  export default Main;
+  
